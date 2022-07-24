1fe0a1a11051d3f416596791b44f685ec34ac8b3
Chaewon Yoon(Jamie) <104494969+jamie7dev@users.noreply.github.com>
2022-07-24 20:12:54 +0900

    Add files via upload

--- dbmovie.py
+++ dbmovie.py
@@ -3,8 +3,20 @@ import certifi
 
 ca = certifi.where()
 
-client = MongoClient('mongodb+srv://test:sparta@cluster0.axdjt.mongodb.net/Cluster0?retryWrites=true&w=majority', tlsCAFile=ca)
+client = MongoClient('mongodb+srv://test:sparta@cluster0.axdjt.mongodb.net/Cluster0?retryWrites=true&w=majority',
+                     tlsCAFile=ca)
 db = client.dbsparta
 
-db.movies.update_one({'title':'가버나움'},{'$set':{'star':'0'}})
+#영화 '가버나움'의 평점을 찾기
+target_movie = db.movies.find_one({'title': '가버나움'})
+target_star = target_movie['star']
+print(target_star)
 
+#'가버나움'과 같은 평점을 갖고 있는 영화 모두 찾기
+movies = list(db.movies.find({'star': target_star}))
+
+for movie in movies:
+    print(movie['title'])
+
+# #영화 '법정'의 평점 0점으로 만들기
+db.movies.update_one({'title': '법정'}, {'$set': {'star': '0'}})
