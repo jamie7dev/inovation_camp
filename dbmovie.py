2615220691c485395f066faa35d32019a28facbf
Chaewon Yoon(Jamie) <104494969+jamie7dev@users.noreply.github.com>
2022-07-24 19:28:00 +0900

    Add files via upload

---
+++ dbmovie.py
@@ -0,0 +1,10 @@
+from pymongo import MongoClient
+import certifi
+
+ca = certifi.where()
+
+client = MongoClient('mongodb+srv://test:sparta@cluster0.axdjt.mongodb.net/Cluster0?retryWrites=true&w=majority', tlsCAFile=ca)
+db = client.dbsparta
+
+db.movies.update_one({'title':'가버나움'},{'$set':{'star':'0'}})
+
