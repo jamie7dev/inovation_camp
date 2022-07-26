95a80da938ceda1955c29c05ee73c112fc31fb31
Chaewon Yoon(Jamie) <104494969+jamie7dev@users.noreply.github.com>
2022-07-26 21:40:21 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,53 +1,36 @@
 from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
-import requests
-from bs4 import BeautifulSoup
-
 from pymongo import MongoClient
 import certifi
 
 ca = certifi.where()
 
-client = MongoClient('mongodb+srv://test:sparta@cluster0.axdjt.mongodb.net/Cluster0?retryWrites=true&w=majority', tlsCAFile=ca)
+client = MongoClient('mongodb+srv://test:sparta@cluster0.axdjt.mongodb.net/Cluster0retryWrites=true&w=majority', tlsCAFile=ca)
 db = client.dbsparta
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+   return render_template('index.html')
 
-
-@app.route("/movie", methods=["POST"])
-def movie_post():
-    url_receive = request.form['url_give']
-    star_receive = request.form['star_give']
-    comment_receive = request.form['comment_give']
-
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-    data = requests.get(url_receive, headers=headers)
-
-    soup = BeautifulSoup(data.text, 'html.parser')
-
-    title = soup.select_one('meta[property="og:title"]')['content']
-    image = soup.select_one('meta[property="og:image"]')['content']
-    desc = soup.select_one('meta[property="og:description"]')['content']
+@app.route("/homework", methods=["POST"])
+def homework_post():
+    name_receive = request.form["name_give"]
+    comment_receive = request.form["comment_give"]
 
     doc = {
-        'title':title,
-        'image':image,
-        'desc':desc,
-        'star':star_receive,
-        'comment':comment_receive
+        'name': name_receive,
+        'comment': comment_receive
     }
-    db.movies.insert_one(doc)
 
-    return jsonify({'msg':'저장 완료!'})
+    db.homework.insert_one(doc)
+    return jsonify({'msg':'응원 완료!'})
 
+@app.route("/homework", methods=["GET"])
+def homework_get():
+    comment_list = list(db.homework.find({},{'_id':False}))
+    return jsonify({'comments':comment_list})
 
-@app.route("/movie", methods=["GET"])
-def movie_get():
-    movie_list = list(db.movies.find({}, {'_id': False}))
-    return jsonify({'movies': movie_list})
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
+   app.run('0.0.0.0', port=5000, debug=True)
