5d5bf22bcafdfe9afa476bb9050933f08955bc52
Chaewon Yoon(Jamie) <104494969+jamie7dev@users.noreply.github.com>
2022-07-27 10:35:05 +0900

    Add files via upload
    이노베이션캠프 입학시험 (22.07.27) - 버킷리스트 취소 기능 구현하기 + 배포하기

--- app.py
+++ app.py
@@ -3,34 +3,50 @@ app = Flask(__name__)
 
 from pymongo import MongoClient
 import certifi
-
 ca = certifi.where()
-
-client = MongoClient('mongodb+srv://test:sparta@cluster0.axdjt.mongodb.net/Cluster0retryWrites=true&w=majority', tlsCAFile=ca)
+client = MongoClient('mongodb+srv://test:sparta@cluster0.axdjt.mongodb.net/Cluster0?retryWrites=true&w=majority', tlsCAFile=ca)
 db = client.dbsparta
+
 
 @app.route('/')
 def home():
-   return render_template('index.html')
+    return render_template('index.html')
 
-@app.route("/homework", methods=["POST"])
-def homework_post():
-    name_receive = request.form["name_give"]
-    comment_receive = request.form["comment_give"]
+
+@app.route("/bucket", methods=["POST"])
+def bucket_post():
+    bucket_receive = request.form['bucket_give']
+
+    bucket_list = list(db.bucket.find({}, {'_id': False}))
+    count = len(bucket_list) + 1
 
     doc = {
-        'name': name_receive,
-        'comment': comment_receive
+        'num':count,
+        'bucket':bucket_receive,
+        'done':0
     }
+    db.bucket.insert_one(doc)
 
-    db.homework.insert_one(doc)
-    return jsonify({'msg':'응원 완료!'})
+    return jsonify({'msg': '등록 완료!'})
 
-@app.route("/homework", methods=["GET"])
-def homework_get():
-    comment_list = list(db.homework.find({},{'_id':False}))
-    return jsonify({'comments':comment_list})
 
+@app.route("/bucket/done", methods=["POST"])
+def bucket_done():
+    num_receive = request.form['num_give']
+    db.bucket.update_one({'num': int(num_receive)}, {'$set': {'done': 1}})
+    return jsonify({'msg': '연결 완료!'})
+
+@app.route("/bucket/cancel", methods=["POST"])
+def bucket_cancel():
+    num_receive = request.form['num_give']
+    db.bucket.update_one({'num': int(num_receive)}, {'$set': {'done': 0}})
+    return jsonify({'msg': '취소 완료!'})
+
+
+@app.route("/bucket", methods=["GET"])
+def bucket_get():
+    bucket_list = list(db.bucket.find({}, {'_id': False}))
+    return jsonify({'buckets': bucket_list})
 
 if __name__ == '__main__':
-   app.run('0.0.0.0', port=5000, debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
